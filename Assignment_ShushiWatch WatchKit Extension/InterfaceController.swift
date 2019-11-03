ff263a2f1b1d8598a5b9246e0b4be7408c27d81e
Nachiketa <nachiketa0333@gmail.com>
2019-11-03 13:11:23 -0500

    Initial Commit

---
+++ Assignment_ShushiWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Assignment_ShushiWatch WatchKit Extension
+//
+//  Created by Nachiketa Nachiketa on 2019-11-03.
+//  Copyright © 2019 Nachiketa Nachiketa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Assignment_ShushiWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment_ShushiWatch
+//
+//  Created by Nachiketa Nachiketa on 2019-11-03.
+//  Copyright © 2019 Nachiketa Nachiketa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
